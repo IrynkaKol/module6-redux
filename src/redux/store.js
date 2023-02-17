864143dc6d395b22ec10e9d83d87f2b8b63e5900
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-02-17 22:46:59 +0100

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,16 @@
 //import { legacy_createStore as createStore} from 'redux'
 //import { devToolsEnhancer } from "@redux-devtools/extension";
 import { configureStore } from "@reduxjs/toolkit";
-import { tasksReducer, filtersReducer  } from "./reducer";
+//import { tasksReducer, filtersReducer  } from "./reducer";
+import { tasksReducer } from "./tasksSlice";
+import { filtersReducer } from "./filtersSlice";
 
 
 export const store = configureStore({
-    reducer: {
-        tasks: tasksReducer,
-        filters: filtersReducer,
-    }
-  });
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
 /*const enhancer = devToolsEnhancer();
 export const store = createStore(rootReducer, enhancer);*/
