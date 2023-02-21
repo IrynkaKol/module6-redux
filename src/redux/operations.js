02ff8b5b9dd85110e9a947001b3daad07e68b80b
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-02-21 22:07:13 +0100

    add async

---
+++ src/redux/operations.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
+
+export const fetchTasks = createAsyncThunk("tasks/fetchAll", 
+async (_, thunkApi) => {
+    try {
+        const response = await axios.get("/tasks");
+        return response.data
+    } catch (e) {
+        return thunkApi.rejectWithValue(e.message)
+
+    }
+
+}
+)
